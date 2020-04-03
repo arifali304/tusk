33191ededfdf528207cd9aa243a3a2561543ff9e
arifali304 <63101888+arifali304@users.noreply.github.com>
2020-04-03 07:53:35 -0500

    Turtle program

--- Create Change Password UI.py
+++ Create Change Password UI.py
@@ -1,1 +1,15 @@
 Welcome to my first python program
+import turtle
+arif_turtle = turtle.Turtle()
+def shape():
+    arif_turtle.forward(1)
+    arif_turtle.right(45)
+    arif_turtle.forward(100)
+    arif_turtle.right(90)
+    arif_turtle.forward(100)
+    arif_turtle.right(90)
+    arif_turtle.forward(100)
+    arif_turtle.right(90)
+    arif_turtle.forward(100)
+for turtle in range(9):
+    shape()
